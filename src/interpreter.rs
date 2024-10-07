c5022feebd2222555ed0725a8845b1b0b36fb5d2
16danjj <16danjj@gmail.com>
2024-10-07 21:05:14 +0200

    section 7.2 - operators

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -11,7 +11,29 @@ impl ExprVisitor<Object> for Interpreter{
     }
 
     fn visit_binary_expr(&self, expr: &BinaryExpr) -> Result<Object, LoxError>{
-        Ok(Object::Nil)
+        let left = self.evaluate(&expr.left)?;
+        let right = self.evaluate(&expr.right)?;
+
+        let result = match expr.operator.token_type() {
+            TokenType::Minus => {left - right},
+            TokenType::Slash => {left / right},
+            TokenType::Star => {left * right},
+            TokenType::Plus => {left + right},
+            TokenType::Greater => Object::Bool(left > right),
+            TokenType::GreaterEqual => Object::Bool(left >= right),
+            TokenType::Less => Object::Bool(left < right),
+            TokenType::LessEqual => Object::Bool(left <= right),
+            TokenType::BangEqual => Object::Bool(left != right),
+            TokenType::Equals => Object::Bool(left == right),
+            _ => {todo!("TBD")}
+        };
+
+        if result == Object::ArithmeticError {
+            Err(LoxError::error(expr.operator.line, "Illegal expression"))
+        } else {
+            Ok(result)
+        }
+        
     }
     fn visit_grouping_expr(&self, expr: &GroupingExpr) -> Result<Object, LoxError>{
         Ok(self.evaluate(&expr.expression)?)
@@ -50,14 +72,22 @@ mod tests {
     use super::*;
     use crate::token::*;
 
+    fn make_literal(o: Object) -> Box<Expr>{
+        Box::new(Expr::Literal(LiteralExpr{
+            value: Some(o)
+        }))
+    }
+
+    fn make_literal_string(s: &str) -> Box<Expr> {
+        make_literal(Object::Str(s.to_string()))
+    }
+
     #[test]
     fn test_unary_minus() {
         let terp = Interpreter {};
         let unary_expr = Expr::Unary(UnaryExpr{
             operator: Token::new(TokenType::Minus, "-".to_string(), None, 0),
-            right: Box::new(Expr::Literal(LiteralExpr{
-                value: Some(Object::Num(123.0))
-            }))
+            right: make_literal(Object::Num(123.0))
         });
 
         let result = terp.evaluate(&unary_expr);
@@ -70,12 +100,191 @@ mod tests {
         let terp = Interpreter {};
         let unary_expr = Expr::Unary(UnaryExpr{
             operator: Token::new(TokenType::Bang, "!".to_string(), None, 0),
-            right: Box::new(Expr::Literal(LiteralExpr{
-                value: Some(Object::Bool(false))
-            }))
+            right: make_literal(Object::Bool(false))
         });
 
         let result = terp.evaluate(&unary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Bool(true)));
+    }
+
+    #[test]
+    fn test_subtraction(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal(Object::Num(15.0)),
+            operator: Token::new(TokenType::Minus, "-".to_string(), None, 0),
+            right: make_literal(Object::Num(7.0))
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Num(8.0)));
+    }
+
+    #[test]
+    fn test_division(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal(Object::Num(21.0)),
+            operator: Token::new(TokenType::Slash, "/".to_string(), None, 0),
+            right: make_literal(Object::Num(7.0))
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Num(3.0)));
+    }
+
+    #[test]
+    fn test_multiplication(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal(Object::Num(15.0)),
+            operator: Token::new(TokenType::Star, "*".to_string(), None, 0),
+            right: make_literal(Object::Num(7.0))
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Num(105.0)));
+    }
+
+    #[test]
+    fn test_addition(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal(Object::Num(21.0)),
+            operator: Token::new(TokenType::Plus, "+".to_string(), None, 0),
+            right: make_literal(Object::Num(7.0))
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Num(28.0)));
+    }
+
+    #[test]
+    fn test_string_concatenation(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal_string("hello, "),
+            operator: Token::new(TokenType::Plus, "+".to_string(), None, 0),
+            right: make_literal_string("world!")
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Str("hello, world!".to_string())));
+    }
+
+    #[test]
+    fn test_arithmetic_error_for_subtraction(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal(Object::Num(15.0)),
+            operator: Token::new(TokenType::Minus, "-".to_string(), None, 0),
+            right: make_literal(Object::Bool(true))
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_greater_than_equal_to(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left:make_literal(Object::Num(15.0)),
+            operator: Token::new(TokenType::GreaterEqual, ">=".to_string(), None, 0),
+            right: make_literal(Object::Num(15.0))
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Bool(true)));
+    }
+
+    #[test]
+    fn test_greater_than_false(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal(Object::Num(15.0)),
+            operator: Token::new(TokenType::Greater, ">".to_string(), None, 0),
+            right: make_literal(Object::Num(17.0))
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Bool(false)));
+    }
+
+    #[test]
+    fn test_equals(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal(Object::Num(15.0)),
+            operator: Token::new(TokenType::Equals, "==".to_string(), None, 0),
+            right: make_literal(Object::Num(15.0))
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Bool(true)));
+    }
+
+    #[test]
+    fn test_not_equals_string(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal_string("hello"),
+            operator: Token::new(TokenType::Equals, "==".to_string(), None, 0),
+            right: make_literal_string("hellx")
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Bool(false)));
+    }
+
+    #[test]
+    fn test_equals_string(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal_string("world"),
+            operator: Token::new(TokenType::Equals, "==".to_string(), None, 0),
+            right: make_literal_string("world")
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Bool(true)));
+    }
+
+    #[test]
+    fn test_equals_nil(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal(Object::Nil),
+            operator: Token::new(TokenType::Equals, "==".to_string(), None, 0),
+            right: make_literal(Object::Nil)
+        });
+
+        let result = terp.evaluate(&binary_expr);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Object::Bool(true)));
+    }
+
+    #[test]
+    fn test_not_equals(){
+        let terp = Interpreter {};
+        let binary_expr = Expr::Binary(BinaryExpr {
+            left: make_literal(Object::Num(15.0)),
+            operator: Token::new(TokenType::BangEqual, "!=".to_string(), None, 0),
+            right: make_literal(Object::Num(16.0))
+        });
+
+        let result = terp.evaluate(&binary_expr);
         assert!(result.is_ok());
         assert_eq!(result.ok(), Some(Object::Bool(true)));
     }
--- src/object.rs
+++ src/object.rs
@@ -1,4 +1,7 @@
   use std::fmt;
+  use std::ops::*;
+  use std::cmp::*;
+
 
   #[derive(Debug, PartialEq, Clone)]
   pub enum Object {
@@ -6,6 +9,7 @@
     Str(String), 
     Bool(bool),
     Nil,
+    ArithmeticError
   }
 
   impl fmt::Display for Object {
@@ -15,8 +19,71 @@
             Object::Str(x) => write!(f, "\"{x}\""),
             Object::Bool(x) => {if *x {write!(f, "true")} else {write!(f, "false")}}
             Object::Nil => write!(f, "nil"),
+            Object::ArithmeticError => panic!("Should not be trying to print this"),
         }
         
+    }
+  }
+
+  impl Sub for Object{
+    type Output = Object;
+
+    fn sub(self, other: Self) -> Object{
+      match (self, other) {
+        (Object::Num(left), Object::Num(right)) => Object::Num(left - right),
+        _ => Object::ArithmeticError,
+      }
+    }
+  }
+
+  impl Div for Object{
+    type Output = Object;
+
+    fn div(self, other: Self) -> Object {
+        match (self, other) {
+          (Object::Num(left), Object::Num(right)) => Object::Num(left / right),
+          _ => Object::ArithmeticError,
+        }
+    }
+  }
+
+
+  impl Mul for Object{
+    type Output = Object;
+
+    fn mul(self, other: Self) -> Object {
+        match (self, other) {
+          (Object::Num(left), Object::Num(right)) => Object::Num(left * right),
+          _ => Object::ArithmeticError,
+        }
+    }
+  }
+
+  impl Add for Object{
+    type Output = Object;
+
+    fn add(self, other: Self) -> Object {
+        match (self, other) {
+          (Object::Num(left), Object::Num(right)) => Object::Num(left + right),
+          (Object::Str(left), Object::Str(right)) => Object::Str(format!("{}{}", left,right)),
+          _ => Object::ArithmeticError,
+        }
+    }
+  }
+
+  impl PartialOrd for Object{
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self, other) {
+          (Object::Nil, o) => {
+            if o == &Object::Nil {
+              Some(Ordering::Equal)
+            } else {
+              None
+            }
+          }
+          (Object::Num(left), Object::Num(right)) => left.partial_cmp(right),
+          _ => None
+        }
     }
   }
   
