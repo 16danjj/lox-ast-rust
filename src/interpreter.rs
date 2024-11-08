dd2569efce5a017d5fc381f129fadebf5d79253d
16danjj <16danjj@gmail.com>
2024-11-08 19:30:27 +0100

    if statement implementation

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,3 @@
-use crate::environment;
 use crate::environment::Environment;
 use crate::error::*;
 use crate::expr::*;
@@ -13,6 +12,16 @@ pub struct Interpreter {
 }
 
 impl StmtVisitor<()> for Interpreter {
+    fn visit_if_stmt(&self, stmt: &IfStmt) -> Result<(), LoxError> {
+        if self.is_truthy(&self.evaluate(&stmt.condition)?) {
+            self.execute(&stmt.then_branch)
+        } else if let Some(else_branch) = &stmt.else_branch {
+            self.execute(else_branch)
+        } else {
+            Ok(())
+        }
+    }
+
     fn visit_block_stmt(&self, stmt: &BlockStmt) -> Result<(), LoxError> {
         let e = Environment::new_with_enclosing(self.environment.borrow().clone());
 
